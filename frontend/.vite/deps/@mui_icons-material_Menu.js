1be6f1c51a147a632e9a671eb446b1a5f80e5481
DannyFlores27 <dannyafloresc@gmail.com>
2025-06-09 03:19:46 -0600

    Secciones.ts

---
+++ frontend/.vite/deps/@mui_icons-material_Menu.js
@@ -0,0 +1,21 @@
+"use client";
+import "./chunk-GQELU2SF.js";
+import {
+  createSvgIcon,
+  require_jsx_runtime
+} from "./chunk-RPOOXQDX.js";
+import "./chunk-OWPSNQWW.js";
+import "./chunk-UPIQUO6F.js";
+import {
+  __toESM
+} from "./chunk-DC5AMYBS.js";
+
+// node_modules/.pnpm/@mui+icons-material@7.1.1_@_54b0ce6b8de693653762102b27d3b6f7/node_modules/@mui/icons-material/esm/Menu.js
+var import_jsx_runtime = __toESM(require_jsx_runtime());
+var Menu_default = createSvgIcon((0, import_jsx_runtime.jsx)("path", {
+  d: "M3 18h18v-2H3zm0-5h18v-2H3zm0-7v2h18V6z"
+}), "Menu");
+export {
+  Menu_default as default
+};
+//# sourceMappingURL=@mui_icons-material_Menu.js.map
--- frontend/src/store/secciones.ts
+++ frontend/src/store/secciones.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand'
-import { type Seccion } from '../types'
+import { type Seccion } from '../types.d'
 
 interface State {
   secciones: Seccion[]
